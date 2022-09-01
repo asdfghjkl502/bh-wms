16b51c3a0902e6a4f6c09be53b77d5e6fb0fdf53
hs <hd1611756908@163.com>
2022-09-01 21:39:05 +0800

    角色测试

--- src/main/java/io/ukoko/bhwms/mapper/RoleMapper.java
+++ src/main/java/io/ukoko/bhwms/mapper/RoleMapper.java
@@ -1,12 +1,14 @@
 package io.ukoko.bhwms.mapper;
 
 import io.ukoko.bhwms.entity.Role;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 /**
  * 操作数据库角色表(role)的接口类
  */
+@Mapper
 public interface RoleMapper {
     /*
         添加角色
@@ -24,7 +26,7 @@ public interface RoleMapper {
     /*
         查询角色列表
      */
-    List<Role> getRoleList();
+    List<Role> getRoleList(Integer isDelete);
     /*
         通过角色ID查询角色信息
      */
---
+++ src/test/java/io/ukoko/bhwms/mapper/RoleMapperTest.java
@@ -0,0 +1,44 @@
+package io.ukoko.bhwms.mapper;
+
+import io.ukoko.bhwms.BhWmsApplicationTests;
+import io.ukoko.bhwms.entity.Role;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoleMapperTest extends BhWmsApplicationTests {
+
+    @Autowired
+    private RoleMapper roleMapper;
+
+    @Test
+    void addRole() {
+        roleMapper.addRole(new Role(null,"库管"));
+    }
+
+    @Test
+    void deleteRole() {
+        roleMapper.deleteRole(1);
+    }
+
+    @Test
+    void updateRole() {
+        Role role = new Role();
+        role.setRoleId(1);
+        role.setRoleName("平台管理员");
+        role.setIsDelete(1);
+        roleMapper.updateRole(role);
+    }
+
+    @Test
+    void getRoleList() {
+        roleMapper.getRoleList(0).forEach(System.out::println);
+    }
+
+    @Test
+    void getRoleBy() {
+        Role role = roleMapper.getRoleBy(1);
+        System.out.println(role);
+    }
+}

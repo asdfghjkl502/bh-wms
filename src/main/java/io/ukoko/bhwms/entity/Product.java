3ba6f7bf56f808d36b100c560fa0820fe1ee068b
hs <hd1611756908@163.com>
2022-09-01 20:42:32 +0800

    引入SQL语句

--- src/main/java/io/ukoko/bhwms/entity/Product.java
+++ src/main/java/io/ukoko/bhwms/entity/Product.java
@@ -1,11 +1,94 @@
 package io.ukoko.bhwms.entity;
 
+/**
+ * 映射数据库product(产品)表
+ */
 public class Product {
-
+  /*
+    产品ID
+   */
   private Integer productId;
+  /*
+    产品名称
+   */
   private String productName;
+  /*
+    类目ID
+   */
   private Integer categoryId;
+  /*
+    产品分类(多对一)
+   */
+  private Category category;
+  /*
+    产品尺寸
+   */
   private String productSize;
+  /*
+    产品价格
+   */
   private Double productPrice;
 
+  public Product() {
+  }
+
+  public Integer getProductId() {
+    return productId;
+  }
+
+  public void setProductId(Integer productId) {
+    this.productId = productId;
+  }
+
+  public String getProductName() {
+    return productName;
+  }
+
+  public void setProductName(String productName) {
+    this.productName = productName;
+  }
+
+  public Integer getCategoryId() {
+    return categoryId;
+  }
+
+  public void setCategoryId(Integer categoryId) {
+    this.categoryId = categoryId;
+  }
+
+  public Category getCategory() {
+    return category;
+  }
+
+  public void setCategory(Category category) {
+    this.category = category;
+  }
+
+  public String getProductSize() {
+    return productSize;
+  }
+
+  public void setProductSize(String productSize) {
+    this.productSize = productSize;
+  }
+
+  public Double getProductPrice() {
+    return productPrice;
+  }
+
+  public void setProductPrice(Double productPrice) {
+    this.productPrice = productPrice;
+  }
+
+  @Override
+  public String toString() {
+    return "Product{" +
+            "productId=" + productId +
+            ", productName='" + productName + '\'' +
+            ", categoryId=" + categoryId +
+            ", category=" + category +
+            ", productSize='" + productSize + '\'' +
+            ", productPrice=" + productPrice +
+            '}';
+  }
 }
--- src/main/java/io/ukoko/bhwms/entity/RecordIn.java
+++ src/main/java/io/ukoko/bhwms/entity/RecordIn.java
@@ -2,9 +2,17 @@ package io.ukoko.bhwms.entity;
 
 import java.util.Date;
 
+/**
+ * 映射数据库record_in(入库记录表)表
+ */
 public class RecordIn {
-
+  /*
+    入库ID
+   */
   private Integer recordInId;
+  /*
+
+   */
   private Integer supplierId;
   private Integer productId;
   private Integer recordInNumber;

3eb75ae479c417ab5073b27b5ea251878a29bdcc
yebo <hd1611756908@163.com>
2022-09-08 08:46:28 +0800

    修改

--- src/main/java/io/ukoko/bhwms/controller/RoleController.java
+++ src/main/java/io/ukoko/bhwms/controller/RoleController.java
@@ -34,15 +34,12 @@ public class RoleController {
      * @return
      */
     @GetMapping(value = "/getRoleList")
-    public Object getRoleList(Integer limit,Integer offset,Integer isDelete){
+    public Result getRoleList(Integer limit,Integer offset,Integer isDelete){
         System.out.println("limit:"+limit);
         System.out.println("offset:"+offset);
         System.out.println("isDelete:"+isDelete);
         Page page = roleService.getRolePageFor(limit,offset,isDelete);
-        Map<String,Object> map = new HashMap<>();
-        map.put("total",page.getCount());
-        map.put("rows",page.getData());
-        return map;
+        return new Result(page);
     }
 
     @GetMapping(value = "/getRoleBootstrap")

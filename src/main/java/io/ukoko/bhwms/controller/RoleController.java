4934c24ce5818188adaf79ac8be4809e158499db
hs <hd1611756908@163.com>
2022-09-14 23:04:02 +0800

    修改

--- src/main/java/io/ukoko/bhwms/controller/RoleController.java
+++ src/main/java/io/ukoko/bhwms/controller/RoleController.java
@@ -1,8 +1,17 @@
 package io.ukoko.bhwms.controller;
 
 import io.swagger.annotations.Api;
+import io.ukoko.bhwms.dto.Page;
+import io.ukoko.bhwms.dto.Result;
+import io.ukoko.bhwms.entity.Role;
+import io.ukoko.bhwms.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @Api(tags = "角色模块")
 @CrossOrigin
@@ -10,7 +19,69 @@ import org.springframework.web.bind.annotation.RestController;
 public class RoleController {
 
 
+    @Autowired
+    private RoleService roleService;
 
+    /**
+     * 添加角色
+     * @param role:角色信息
+     * @return
+     */
+    @PostMapping(value = "/addRole")
+    public Object addRole(Role role){
+        System.out.println("添加角色: "+role);
+        roleService.addRole(role);
+        return new Result();
+    }
 
+    /**
+     * 删除角色
+     * @param roleId:角色ID
+     * @return
+     */
+    @GetMapping(value = "/deleteRoleByRoleId")
+    public Object deleteRoleByRoleId(Integer roleId){
+        System.out.println("删除角色: "+roleId);
+        return new Result();
+    }
 
+    /**
+     * 更新角色
+     * @param role
+     * @return
+     */
+    @PostMapping(value = "/updateRole")
+    public Object updateRole(Role role){
+        System.out.println("更新角色: "+role);
+        roleService.updateRole(role);
+        return new Result();
+    }
+
+    /**
+     * 获取角色列表信息
+     * @param limit : 每页显示多少条数
+     * @param offset: 偏移量
+     * @param isDelete : 是否删除
+     * @param roleName : 角色名称
+     * @param startTime: 开始时间
+     * @param endTime: 结束时间
+     * @param roleId : 角色ID
+     * @return
+     */
+    @GetMapping(value = "/getRoles")
+    public Result getRoles(int limit, int offset, Integer isDelete, String roleName, Date startTime, Date endTime, Integer roleId){
+        Page page = roleService.getRolePageFor(limit, offset, isDelete, roleName, startTime, endTime, roleId);
+        return new Result(page);
+    }
+
+    /**
+     * 获取角色信息
+     * @param roleId:角色ID
+     * @return
+     */
+    @GetMapping(value = "/getRole")
+    public Object getRole(Integer roleId){
+        Role role = roleService.getRoleByRoleId(roleId);
+        return new Result(role);
+    }
 }

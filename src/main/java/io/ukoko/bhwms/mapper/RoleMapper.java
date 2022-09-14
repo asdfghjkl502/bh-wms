c92585fa1b124256385376334beb68b93e3948ea
夜泊 <hd1611756908@163.com>
2022-09-14 10:34:43 +0800

    role mapper注册

--- src/main/java/io/ukoko/bhwms/mapper/RoleMapper.java
+++ src/main/java/io/ukoko/bhwms/mapper/RoleMapper.java
@@ -25,29 +25,19 @@ public interface RoleMapper {
         更新角色
      */
     void updateRole(Role role);
-    /*
-        查询角色列表
-        isDelete: 是否删除 1: 未删除 0:已删除
-     */
-    List<Role> getRoleList(Integer isDelete);
+
     /*
         通过角色ID查询角色信息
      */
-    Role getRoleBy(Integer roleId);
+    Role getRoleByRoleId(Integer roleId);
 
     /**
-     * 搜索
-     * @param roleName: 搜索条件
-     * @param isDelete: 是否删除 1: 未删除 0:已删除
-     * @return
-     */
-    List<Role> getRoleListLike(@Param("roleName") String roleName,@Param("isDelete") Integer isDelete);
-
-    List<Role> getRolesByParam(@Param("roleName") String roleName, @Param("isDelete") Integer isDelete, @Param("startTime") Date startTime,@Param("endTime")Date endTime);
-
-    /**
-     * 批量删除
-     * @param roleIds
+         批量删除
      */
     void batchDeleteRole(List<Integer> roleIds);
+
+    /*
+        查询角色列表
+     */
+    List<Role> getRoleList(@Param("roleName") String roleName,@Param("isDelete")Integer isDelete,@Param("roleId")Integer roleId,@Param("startTime")Date startTime,@Param("endTime")Date endTime);
 }

c26a3044c15c0c6b110efce07d3703fd6a5a622b
夜泊 <hd1611756908@163.com>
2022-09-19 13:37:49 +0800

    客户测试

---
+++ src/test/java/io/ukoko/bhwms/service/CustomerServiceTest.java
@@ -0,0 +1,25 @@
+package io.ukoko.bhwms.service;
+
+import io.ukoko.bhwms.BhWmsApplicationTests;
+import io.ukoko.bhwms.dto.Page;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerServiceTest extends BhWmsApplicationTests {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Test
+    void addCustomer() {
+    }
+
+    @Test
+    void getCustomerPage() {
+        Page page = customerService.getCustomerPage(0, 2, null, null, null, null, null, null, null, null);
+        Object data = page.getData();
+        System.out.println(data);
+    }
+}

8309b3aac474d5018bae6004cc8d57de56684556
hs <hd1611756908@163.com>
2022-11-04 22:13:30 +0800

    添加日志

---
+++ src/main/java/io/ukoko/bhwms/aop/LogAspect.java
@@ -0,0 +1,51 @@
+package io.ukoko.bhwms.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 日志切面
+ */
+@Aspect
+@Component
+public class LogAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger("BH-WMS");
+
+    /**
+     * 统一日志
+     */
+    @Pointcut(value = "execution(* io.ukoko.bhwms.controller.*.*(..))")
+    public void log(){}
+
+    /**
+     * 环绕通知
+     */
+    @Around(value = "log()")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object proceed = joinPoint.proceed();
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = requestAttributes.getRequest();
+        //获取主机IP地址
+        LOGGER.info("客户端IP地址==>>{}",request.getRemoteAddr());
+        //获取请求客户端地址
+        LOGGER.info("请求URL地址==>>{}",request.getRequestURL().toString());
+        //获取请求方式
+        LOGGER.info("请求方式==>>{}",request.getMethod());
+        //获取类名和方法名
+        LOGGER.info("类名==>>{} |￥￥| 方法名==>>{}",joinPoint.getSignature().getDeclaringTypeName(),joinPoint.getSignature().getName());
+        //获取结果
+        LOGGER.info("方法返回值为 ==>>{}",proceed);
+        return proceed;
+    }
+
+}
--- src/main/java/io/ukoko/bhwms/handlers/BhWmsExceptionHandler.java
+++ src/main/java/io/ukoko/bhwms/handlers/BhWmsExceptionHandler.java
@@ -1,7 +1,11 @@
 package io.ukoko.bhwms.handlers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.ukoko.bhwms.dto.Result;
 import io.ukoko.bhwms.exceptions.BhWmsException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ControllerAdvice
 public class BhWmsExceptionHandler {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger("BH-WMS");
 
     @ExceptionHandler(value = {BhWmsException.class})
     public Result departmentException(BhWmsException e){
@@ -20,6 +25,13 @@ public class BhWmsExceptionHandler {
         Result result = new Result();
         result.setCode(e.getCode());
         result.setMsg(e.getMessage());
+        ObjectMapper om = new ObjectMapper();
+        try {
+            String json = om.writeValueAsString(result);
+            LOGGER.error("异常消息为:{}",json);
+        } catch (JsonProcessingException ex) {
+            LOGGER.error("JSON解析异常:{}",ex.getMessage());
+        }
         return result;
     }
 
@@ -29,6 +41,13 @@ public class BhWmsExceptionHandler {
         Result result = new Result();
         result.setCode(-1);
         result.setMsg("系统异常");
+        ObjectMapper om = new ObjectMapper();
+        try {
+            String json = om.writeValueAsString(result);
+            LOGGER.error("异常消息为:{}",json);
+        } catch (JsonProcessingException ex) {
+            LOGGER.error("JSON解析异常:{}",ex.getMessage());
+        }
         return result;
     }
 

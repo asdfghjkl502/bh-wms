29588db3c30470a4461dbfe04f4dbd90d5d1ee9a
hs <hd1611756908@163.com>
2022-11-08 22:12:26 +0800

    控制层source完成

--- src/main/java/io/ukoko/bhwms/controller/SourceController.java
+++ src/main/java/io/ukoko/bhwms/controller/SourceController.java
@@ -8,10 +8,9 @@ import io.ukoko.bhwms.entity.Source;
 import io.ukoko.bhwms.service.LevelService;
 import io.ukoko.bhwms.service.SourceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Api(tags = "来源模块")
 @CrossOrigin
@@ -19,6 +18,41 @@ import org.springframework.web.bind.annotation.RestController;
 public class SourceController {
     @Autowired
     private SourceService sourceService;
+
+
+    /**
+     * 更新
+     * @param source
+     * @return
+     */
+    @PostMapping(value = "/updateSource")
+    public Object updateSource(@RequestBody Source source){
+        sourceService.updateSource(source);
+        return new Result();
+    }
+
+    /**
+     * 删除
+     * @param sourceId
+     * @return
+     */
+    @GetMapping(value = "/deleteSource")
+    public Object deleteSource(Integer sourceId){
+        sourceService.deleteSourceBySourceId(sourceId);
+        return new Result();
+    }
+
+    /**
+     * 批量删除
+     * @param sourceIds
+     * @return
+     */
+    @PostMapping(value = "/batchDeleteSource")
+    public Object batchDeleteSource(List<Integer> sourceIds){
+        sourceService.batchDeleteSourceBySourceId(sourceIds);
+        return new Result();
+    }
+
 
     /**
      * 来源分页查询

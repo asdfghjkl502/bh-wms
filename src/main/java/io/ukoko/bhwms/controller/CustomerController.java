e4612e2fad3b1a69159f433295527af14e2e7f4c
hs <hd1611756908@163.com>
2022-11-12 22:41:18 +0800

    客户控制器完成

--- src/main/java/io/ukoko/bhwms/controller/CustomerController.java
+++ src/main/java/io/ukoko/bhwms/controller/CustomerController.java
@@ -6,12 +6,10 @@ import io.ukoko.bhwms.dto.Result;
 import io.ukoko.bhwms.entity.Customer;
 import io.ukoko.bhwms.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
 
 @Api(tags = "客户模块")
 @CrossOrigin
@@ -27,7 +25,7 @@ public class CustomerController {
      * @return
      */
     @PostMapping(value = "/addCustomer")
-    public Object addCustomer(Customer customer){
+    public Object addCustomer(@RequestBody Customer customer){
         customerService.addCustomer(customer);
         return new Result();
     }
@@ -49,5 +47,38 @@ public class CustomerController {
     public Object getCustomerPage(int pageNo, int pageSize,String customerName,String customerLeader,String customerTel,String customerEmail,String customerAddress,Date startTime,Date endTime){
         Page page = customerService.getCustomerPage(pageNo, pageSize, customerName, customerLeader, customerTel, customerEmail, customerAddress, startTime, endTime);
         return new Result(page);
+    }
+
+    /**
+     * 更新客户
+     * @param customer
+     * @return
+     */
+    @PostMapping(value = "/updateCustomer")
+    public Object updateCustomer(@RequestBody Customer customer){
+        customerService.updateCustomer(customer);
+        return new Result();
+    }
+
+    /**
+     * 删除
+     * @param customerId
+     * @return
+     */
+    @PostMapping(value = "/deleteCustomer")
+    public Object deleteCustomer(Integer customerId){
+        customerService.deleteCustomer(customerId);
+        return new Result();
+    }
+
+    /**
+     * 批量删除
+     * @param customerIds
+     * @return
+     */
+    @PostMapping(value = "/batchDeleteCustomer")
+    public Object batchDeleteCustomer(List<Integer> customerIds){
+        customerService.batchDeleteCustomer(customerIds);
+        return new Result();
     }
 }

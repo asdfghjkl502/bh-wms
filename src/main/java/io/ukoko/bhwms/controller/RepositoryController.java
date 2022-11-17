94afaa82ec67455b542f12bd01ef2951a5cc7d69
hs <hd1611756908@163.com>
2022-11-17 20:09:03 +0800

    仓库控制器层完成

---
+++ src/main/java/io/ukoko/bhwms/controller/RepositoryController.java
@@ -0,0 +1,84 @@
+package io.ukoko.bhwms.controller;
+
+import io.swagger.annotations.Api;
+import io.ukoko.bhwms.dto.Page;
+import io.ukoko.bhwms.dto.Result;
+import io.ukoko.bhwms.entity.Repository;
+import io.ukoko.bhwms.service.RepositoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
+
+@Api(tags = "仓库模块")
+@CrossOrigin
+@RestController
+public class RepositoryController {
+
+    @Autowired
+    private RepositoryService repositoryService;
+
+    /**
+     * 创建仓库
+     * @param repository
+     * @return
+     */
+    @PostMapping(value = "/addRepository")
+    public Object addRepository(@RequestBody Repository repository){
+        repositoryService.addRepository(repository);
+        return new Result();
+    }
+
+    /**
+     * 更新仓库
+     * @param repository
+     * @return
+     */
+    @PostMapping(value = "/updateRepository")
+    public Object updateRepository(@RequestBody Repository repository){
+        repositoryService.updateRepository(repository);
+        return new Result();
+    }
+
+    /**
+     * 获取仓库列表
+     * @param pageNo
+     * @param pageSize
+     * @param repoAddress
+     * @param startTime
+     * @param endTime
+     * @return
+     */
+    @PostMapping(value = "/getRepositoryPage")
+    public Object getRepositoryPage(Integer pageNo, Integer pageSize, String repoAddress, @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date startTime,@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date endTime){
+        Page page = repositoryService.getRepositoryPage(pageNo, pageSize, repoAddress, startTime, endTime);
+        return new Result(page);
+    }
+
+    /**
+     * 删除仓库
+     * @param repoId
+     * @return
+     */
+    @PostMapping(value = "/deleteRepository")
+    public Object deleteRepository(Integer repoId){
+        repositoryService.deleteRepository(repoId);
+        return new Result();
+    }
+
+    /**
+     * 批量删除仓库
+     * @param repoIds
+     * @return
+     */
+    @PostMapping(value = "/batchDeleteRepository")
+    public Object batchDeleteRepository(@RequestBody List<Integer> repoIds){
+        repositoryService.batchDeleteRepository(repoIds);
+        return new Result();
+    }
+}

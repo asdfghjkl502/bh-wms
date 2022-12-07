d79ae2232cdd0c317d102c9fef08d747e47176a9
夜泊 <hd1611756908@163.com>
2022-12-07 16:03:44 +0800

    创建产品vo类,向前端推送产品信息数据

---
+++ src/main/java/io/ukoko/bhwms/dto/ProductVo.java
@@ -0,0 +1,149 @@
+package io.ukoko.bhwms.dto;
+
+import io.ukoko.bhwms.entity.Category;
+
+import java.util.Date;
+
+/**
+ * 向前端推送产品数据的Java类
+ */
+public class ProductVo {
+
+    /*
+   产品ID
+  */
+    private Integer productId;
+    /*
+      产品名称
+     */
+    private String productName;
+    /*
+      类目ID
+     */
+    private Integer categoryId;
+    /*
+      产品分类(多对一)
+     */
+    private Category category;
+    /*
+      产品尺寸
+     */
+    private String productSize;
+    /*
+      产品价格
+     */
+    private Double productPrice;
+    /*
+          创建时间
+       */
+    private Date createTime;
+    /*
+        更新时间
+     */
+    private Date updateTime;
+    /*
+        是否删除 0:删除 1:未删除
+     */
+    private Integer isDelete=1;
+
+    /*
+        产品库存
+     */
+    private long stock;
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getProductSize() {
+        return productSize;
+    }
+
+    public void setProductSize(String productSize) {
+        this.productSize = productSize;
+    }
+
+    public Double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(Double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Integer getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(Integer isDelete) {
+        this.isDelete = isDelete;
+    }
+
+    public long getStock() {
+        return stock;
+    }
+
+    public void setStock(long stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductVo{" +
+                "productId=" + productId +
+                ", productName='" + productName + '\'' +
+                ", categoryId=" + categoryId +
+                ", category=" + category +
+                ", productSize='" + productSize + '\'' +
+                ", productPrice=" + productPrice +
+                ", createTime=" + createTime +
+                ", updateTime=" + updateTime +
+                ", isDelete=" + isDelete +
+                ", stock=" + stock +
+                '}';
+    }
+}

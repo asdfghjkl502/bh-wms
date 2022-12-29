dd5c44e10e20d66949c9f194e27977f4e909d896
夜泊 <hd1611756908@163.com>
2022-12-29 11:20:18 +0800

    日志查询mapper完成

--- src/main/java/io/ukoko/bhwms/mapper/SysLogMapper.java
+++ src/main/java/io/ukoko/bhwms/mapper/SysLogMapper.java
@@ -2,6 +2,10 @@ package io.ukoko.bhwms.mapper;
 
 import io.ukoko.bhwms.entity.SysLog;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * 操作数据库日志表(sys_log)的接口类
@@ -14,4 +18,20 @@ public interface SysLogMapper {
      */
     void addSysLog(SysLog sysLog);
 
+    /**
+     * 查询日志
+     * @param type
+     * @param methodName
+     * @param method
+     * @param nickName
+     * @param start
+     * @param end
+     * @return
+     */
+    List<SysLog> getSysLogList(@Param("type") Integer type,
+                               @Param("methodName") String methodName,
+                               @Param("method") String method,
+                               @Param("nickName") String nickName,
+                               @Param("start") Date start,
+                               @Param("end") Date end);
 }

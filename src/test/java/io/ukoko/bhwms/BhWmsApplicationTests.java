a9b5a57903e6f321bc93d544c3e73dadcee0b370
夜泊 <hd1611756908@163.com>
2022-12-29 11:39:06 +0800

    编码和解码

--- src/test/java/io/ukoko/bhwms/BhWmsApplicationTests.java
+++ src/test/java/io/ukoko/bhwms/BhWmsApplicationTests.java
@@ -7,7 +7,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+
 @SpringBootTest
 public class BhWmsApplicationTests {
+
+
+    @Test
+    public void test01() throws UnsupportedEncodingException {
+
+        String s="admin";
+
+        //编码
+        String encode = Base64.getEncoder().encodeToString(s.getBytes());
+        System.out.println(encode); //YWRtaW4=
+        //解码
+        byte[] decode = Base64.getDecoder().decode("YWRtaW4=");
+        System.out.println(new String(decode,"UTF-8"));
+
+    }
 
 }
